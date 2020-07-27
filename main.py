3b09f0a2926811f499e31fe9b55fe5ae043cfa68
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-07-27 13:06:03 +0200

    Add no space condition

--- main.py
+++ main.py
@@ -6,12 +6,22 @@
 #Podpowiedź
 #Pamiętaj, że string/tekst, to kolekcja znaków. Znasz już funkcje kolekcji, które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
 
-def palindrome(x)
-    x = input("Czy to palindrom? Podaj tekst: ")
-    for  i in x:
+x = input("Czy to palindrom? Podaj tekst: ")
+def palindrome(x):
+    for  i in str(x):
         y = [i for i in x]
-        z = [i for i in reversed(x)]   
+        for j in y:
+            if j == ' ':
+                y.remove(j)
+        z = [i for i in reversed(x)]
+        for k in z:
+            if k == ' ':
+                z.remove(k)
+    print(y)
+    print(z)
     if y == z:
         print("Podane przez Ciebie wyrażenie to palindrom")
     else:
         print("Podane przez Ciebie wyrażenie nie jest palindromem")
+
+palindrome(x)        
