8db194affe74837c61686d931495aad5f2d74f28
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-07-27 13:06:37 +0200

    Delete instructions

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#Twoim zadaniem będzie napisanie funkcji, która sprawdza, czy dany wyraz jest palindromem.
-# Palindrom to słowo, które czytane od lewej do prawej i od prawej do lewej brzmi tak samo. Przykłady to “kajak” i “potop”.
-
-#Zaprogramuj funkcję, która przyjmuje jeden argument (typu string) i zwraca wartość boolean: True/False, mówiącą czy podany tekst jest palindromem.
-
-#Podpowiedź
-#Pamiętaj, że string/tekst, to kolekcja znaków. Znasz już funkcje kolekcji, które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
-
 x = input("Czy to palindrom? Podaj tekst: ")
 def palindrome(x):
     for  i in str(x):
