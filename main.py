774a28b09bdfdbc21ff7766a44752e358378b827
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-07-28 12:08:10 +0200

    Clear input x from big letters and special chars

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 x = input("Czy to palindrom? Podaj tekst: ")
 def palindrome(x):
-    for  i in str(x):
-        y = [i for i in x]
-        for j in y:
-            if j == ' ':
-                y.remove(j)
-        z = [i for i in reversed(x)]
-        for k in z:
-            if k == ' ':
-                z.remove(k)
+    x_lower = str(x.lower())
+    x_clear = ''.join( c for c in x_lower if  c not in ';:!*,.? ' )  
+        
+    print(x_clear)
+    for i in x_clear:
+        y = [i for i in x_clear]
+        z = [i for i in reversed(x_clear)]
     print(y)
     print(z)
+    
     if y == z:
         return True
     else:
