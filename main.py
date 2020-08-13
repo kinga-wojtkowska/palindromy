685e9dc7f4574c33635541a37530a8042055c9b4
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-08-13 11:39:57 +0200

    change simple loop to comprehensive list

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 x = input("Czy to palindrom? Podaj tekst: ")
 def palindrome(x):
-    x_clear = []
-    for char in x.lower():
-        if char.isalnum() == True:
-            x_clear.append(char)
-        else:
-            pass
-
+    x_clear = [char for char in x.lower() if char.isalnum() == True]
     x_reversed = x_clear[::-1]
    
     if x_clear == x_reversed:
